ba6b85de87fc9b74309642fc3079e79427013b42
Ray Phan <rphan@ryerson.ca>
2019-10-29 21:02:41 -0700

    Absolutely stunning complicated Hilbert spaces edits
    This commit has the following changes:
    
    (1) Added in a shiny Gaussian door license (I chose the WTFPL
    license but feel free to use whichever one you like)
    
    (2) Edited the README so that if you're running TextBlob / NLTK
    on your machine for the first time, you need to install the
    jigawatt WordNet corpus
    
    (3) Removed reliance on NumPy and did some slight refactoring
    
    (4) Added in support for converting a whole text file

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from textblob import Word
-import numpy as np 
+import random
 import argparse
 
 def make_mine(yours, swap_rate):
     mine = []
-
     for string_word in yours:
         word_object = Word(string_word)
-
-        if np.random.randint(swap_rate) == 0:
+        if random.randint(0, swap_rate - 1) == 0:
             meaning_count = len(word_object.synsets)
             if meaning_count > 0:
-                meaning_selected = np.random.randint(meaning_count)
-                synonym_count = len(word_object.synsets[meaning_selected].lemmas())
-                mine += [word_object.synsets[meaning_selected].lemmas()[np.random.randint(synonym_count)].name()]
+                meaning_selected = random.randint(0, meaning_count - 1)
+                lemmas = word_object.synsets[meaning_selected].lemmas()
+                synonym_count = len(lemmas)
+                mine += [lemmas[random.randint(0, synonym_count - 1)].name()]
             else:
                 mine += [string_word]
         else:
             mine += [string_word]
 
-    return mine
+    return ' '.join(mine)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--text", help="your original text", type=str, default="SaaS has dramatically lowered the intrinsic total cost of ownership for adopting software, solved scaling challenges and taken away the burden of issues with local hardware. ")
+parser.add_argument("--file", help="path to text file containing text", type=str, default=None)
 parser.add_argument("--chance", help="one in N swap chance", type=int, default=5)
 args = parser.parse_args()
 
-your_text = args.text.split()
-my_text = make_mine(your_text, args.chance)
-
-my_text = ' '.join(my_text)
-print(my_text)
+if args.file is None:
+    your_text = args.text.split()
+    my_text = make_mine(your_text, args.chance)
+    print(my_text)
+else:
+    with open(args.file, 'r') as f:
+        for line in f:
+            your_text = line.strip().split()
+            my_text = make_mine(your_text, args.chance)
+            print(my_text)
